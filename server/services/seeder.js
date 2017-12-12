24c95063f1fb7c3b4d5f7d8b457efdd0b270f5bd
Miloš Tomšik <milos.tomsik@quantox.com>
2017-12-12 14:23:58 +0100

    implemented primitive test seeder

--- server/services/seeder.js
+++ server/services/seeder.js
@@ -1,8 +1,35 @@
 'use strict';
-import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/getbike');
+function print(text) {
+  // console.log('\n//----------------------------------------------------');
+  // console.log('// ' + text);
+  // console.log('//----------------------------------------------------');
+  console.log('\n' + text);
+}
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/getbike', { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 // -- TODO: ADD SEEDER
+print('Seeding Users...');
+var User = mongoose.model('User', new mongoose.Schema({ name: { type: String } }, { versionKey: false }));
 
-console.log('Seed successfully completed. Disconnecting MongoDB.')
-mongoose.disconnect();
+var counter = 0;
+for (let i = 1; i <= 10; i++) {
+  try {
+    User.create({ name: 'Name ' + i }, function (err, doc) {
+      if (err) {
+        throw new Error('Error writing data to database.');
+      } else {
+        console.log(doc);
+        counter++;
+        if (counter === 10) {
+          print('Seed successfully completed. Disconnecting MongoDB.');
+          mongoose.disconnect();
+        }
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
