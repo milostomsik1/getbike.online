c098d4dd16a6e934a65a11c3f7d3a2fa3b5bc3b2
Miloš Tomšik <milos.tomsik@quantox.com>
2017-12-12 15:57:17 +0100

    seeder changes

--- server/services/seeder.js
+++ server/services/seeder.js
@@ -1,31 +1,32 @@
 'use strict';
-function print(text) {
-  // console.log('\n//----------------------------------------------------');
-  // console.log('// ' + text);
-  // console.log('//----------------------------------------------------');
-  console.log('\n' + text);
-}
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/getbike', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 // -- TODO: ADD SEEDER
-print('Seeding Users...');
+console.log('Seeding Users...');
 var User = mongoose.model('User', new mongoose.Schema({ name: { type: String } }, { versionKey: false }));
 
 var counter = 0;
-for (let i = 1; i <= 10; i++) {
+var amount = 100;
+
+User.collection.drop()
+
+for (let i = 1; i <= amount; i++) {
   try {
     User.create({ name: 'Name ' + i }, function (err, doc) {
       if (err) {
         throw new Error('Error writing data to database.');
       } else {
-        console.log(doc);
         counter++;
-        if (counter === 10) {
-          print('Seed successfully completed. Disconnecting MongoDB.');
-          mongoose.disconnect();
+        if (counter === amount) {
+          User.find({}, function (err, doc) {
+            if (err) throw new Error(err);
+            console.log('Total docs: ' + doc.length);
+            console.log('Seed successfully completed.');
+            mongoose.disconnect();
+          });
         }
       }
     });
@@ -33,3 +34,4 @@ for (let i = 1; i <= 10; i++) {
     console.log(err);
   }
 }
+
