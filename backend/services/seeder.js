5d7a6ed66c2d53274ace137f025d593a8eda441f
Milos Tomsik <milostomsik1@gmail.com>
2018-01-06 14:17:41 +0100

    added array transform

--- backend/services/seeder.js
+++ backend/services/seeder.js
@@ -9,6 +9,30 @@ function generateSeedable(factory) {
   }
 }
 
+function transformDocuments(documents) {
+  documents.sort(function(a, b) {
+    var comparison = 0;
+    comparison = a.key > b.key ? 1 : -1;
+    return comparison;
+  });
+
+  var currKey = documents[0].key;
+  var transformedDocuments = [{key: documents[0].key, value: []}];
+  var lastKeyIndex = 0;
+
+  documents.forEach(function(item, i) {
+    if (item.key === currKey) {
+      transformedDocuments[lastKeyIndex].value.push(item.value);
+    } else {
+      currKey = item.key;
+      lastKeyIndex++;
+      transformedDocuments.push({key: item.key, value: [item.value]});
+    }
+  });
+
+  return transformedDocuments;
+}
+
 export default function seed(factory) {
   return new Promise((resolve, reject) => {
     mongoose.connect('mongodb://localhost/getbike', { useMongoClient: true }).then(() => {
@@ -25,18 +49,18 @@ export default function seed(factory) {
         seedable.references.forEach(reference => {
           let refs = [];
           docs.forEach(doc => {
-            console.log(doc);
-            refs.push({key: doc[reference.name], value: doc._id})
+            refs.push({key: doc[reference.name], value: doc._id});
             // console.log('ad id', doc._id);
             // console.log('seller id', doc[reference.name]);
             // console.log('references', reference.ref);
             // console.log('reference model', reference.model.modelName);
-            reference.model.findById(doc[reference.name]).then(refDoc => {
-              const updatedValue = refDoc[reference.ref] ? refDoc[reference.ref].push(doc._id) : [doc._id];
-              reference.model.findByIdAndUpdate(doc[reference.name], {[reference.ref]: updatedValue})
-              .then(updated => {});
-            });
+            // reference.model.findById(doc[reference.name]).then(refDoc => {
+            //   const updatedValue = refDoc[reference.ref] ? refDoc[reference.ref].push(doc._id) : [doc._id];
+            //   reference.model.findByIdAndUpdate(doc[reference.name], {[reference.ref]: updatedValue})
+            //   .then(updated => {});
+            // });
           });
+          refs = transformDocuments(refs);
           console.log(refs);
           console.log('// -- Should insert ' + seedable.model.modelName + 's into ' + reference.model.modelName + ' here.');
         });
