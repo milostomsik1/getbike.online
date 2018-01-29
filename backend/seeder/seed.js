93be329d94fd2d713069fe9cf4176c94ab46d370
Milos Tomsik <milostomsik1@gmail.com>
2018-01-29 11:03:35 +0100

    fixed dependency problems with seeding

--- backend/seeder/seed.js
+++ backend/seeder/seed.js
@@ -12,17 +12,56 @@ const getAllSeedableFiles = () => {
   const files = fs.readdirSync(path.resolve(__dirname));
   return files.filter(file => isFileSeedable(file))
               .map(file => require(`./${file}`));
+};
+
+
+const findOne = (toCheck, search) => {
+  if (search.length > toCheck.length) {
+    return false;
+  } else {
+    let counter = 0;
+    toCheck.forEach(item => {
+      if (search.includes(item)) {
+        counter++;
+      }
+    });
+    if (counter === search.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+
+const sortByDependencies = files => {
+  const proper = [];
+  const properModels = [];
+  while (files.length > 0) {
+    files.forEach((file, i) => {
+      if (file.dependsOn.length === 0 || findOne(properModels, file.dependsOn)) {
+        proper.push(file);
+        properModels.push(file.modelName);
+        files.splice(i, 1);
+      }
+    });
+  }
+  return proper;
 }
 
 const seed = async () => {
-  const files = getAllSeedableFiles();
+  const files = sortByDependencies(getAllSeedableFiles());
   for (const file of files) {
     const model = file.modelName;
     const { data } = file;
-    await db[model].bulkCreate(data);
-    sequelize.close();
+    try {
+      await db[model].bulkCreate(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+  sequelize.close();
+};
 
 sequelize.sync({force: true}).then(() => {
   seed();
